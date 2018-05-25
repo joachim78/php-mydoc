784af11299989986d5ffebba7334a057f763f0b1
Joachim Deneumostier <jmdr@rtbf.be>
2018-05-25 15:25:57 +0200

    change encoding

--- 01 - basics - tags.php
+++ 01 - basics - tags.php
@@ -1,10 +1,7 @@
 <?php
-  print 'TEST 1 tag <br>';  // ! Les balises d'ouverture et fermeture " < ?php ? >" dans une string => ne seront pas affichées à l'écran (ni interprétées)
+  print 'TEST 1 tag <br>';  // ! Les balises d'ouverture et fermeture " < ?php ? >" dans une string => ne seront pas affichÃ©es Ã  l'Ã©cran (ni interprÃ©tÃ©es)
 ?>
 
  <script language='php'>
-  print "TEST 2 tag <br>";  // Les short tags et ASP-like sont désactivés par défaut
+  print "TEST 2 tag <br>";  // Les short tags et ASP-like sont dÃ©sactivÃ©s par dÃ©faut
  </script>
- 
-
- 
